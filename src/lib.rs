a3338852386a878a788f26c1101106ecab6864e2
Adam Coggeshall <adam@cogg.rocks>
2024-12-01 03:24:21 -0700

    slow sort

--- src/lib.rs
+++ src/lib.rs
@@ -4,33 +4,7 @@ pub mod day1 {
     use core::{assert_eq, iter::Iterator};
     use std::time::Instant;
 
-    struct MapIter<'a> {
-        map: &'a mut [u16],
-        index: usize,
-    }
-
-    impl<'a> MapIter<'a> {
-        pub fn new(map: &'a mut [u16]) -> Self {
-            Self { map, index: 10000 }
-        }
-
-        pub fn next(&mut self) -> Option<usize> {
-            loop {
-                if self.index >= self.map.len() {
-                    return None;
-                }
-
-                if self.map[self.index] > 0 {
-                    self.map[self.index] -= 1;
-                    return Some(self.index);
-                }
-
-                self.index += 1;
-            }
-        }
-    }
-
-    unsafe fn parse_int(bytes: &[u8]) -> i32 {
+    fn parse_int(bytes: &[u8]) -> i32 {
         let a = (bytes[0] - 0x30) as i32 * 10000;
         let b = (bytes[1] - 0x30) as i32 * 1000;
         let c = (bytes[2] - 0x30) as i32 * 100;
@@ -40,45 +14,28 @@ pub mod day1 {
     }
 
     pub fn part1(input: &str) -> i32 {
-        //let mut first: Vec<i32> = Vec::with_capacity(1000);
-        //let mut second: Vec<i32> = Vec::with_capacity(1000);
-
-        let t = Instant::now();
-        let mut first: Vec<u16> = vec![0; 100_100];
-        let mut second: Vec<u16> = vec![0; 100_100];
-        //println!("??? {:?}",t.elapsed());
+        let mut first: Vec<i32> = Vec::with_capacity(1000);
+        let mut second: Vec<i32> = Vec::with_capacity(1000);
 
         // parse
         for row in input.bytes().array_chunks::<14>() {
-            let num1 = unsafe { parse_int(&row[0..5]) };
-            let num2 = unsafe { parse_int(&row[8..13]) };
+            let num1 = parse_int(&row[0..5]);
+            let num2 = parse_int(&row[8..13]);
 
-            first[num1 as usize] += 1;
-            second[num2 as usize] += 1;
+            first.push(num1);
+            second.push(num2);
         }
 
-        let mut map_1 = MapIter::new(&mut first);
-        let mut map_2 = MapIter::new(&mut second);
-
-        /*let mut sumx = 0;
-        for i in first {
-            sumx += i;
-        }*/
-
-        //1258579
         // sort
-        /*{
+        {
             first.sort();
             second.sort();
-        }*/
+        }
 
         // sum
         let mut sum = 0;
-        loop {
-            let Some(a) = map_1.next() else { break };
-            let Some(b) = map_2.next() else { break };
-
-            sum += (a as i32 - b as i32).abs();
+        for (a, b) in first.iter().zip(second) {
+            sum += (a - b).abs();
         }
 
         sum
