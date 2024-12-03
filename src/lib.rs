e50a3c9a34eb7dcf178355c66a9f4ff15dc46ead
Adam Coggeshall <adam@cogg.rocks>
2024-12-02 23:59:48 -0700

    day2, part 2

--- src/lib.rs
+++ src/lib.rs
@@ -175,25 +175,25 @@ pub mod day2 {
     }
 
     #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
-    unsafe fn check_fast(array: &[i8;8]) -> bool {
+    unsafe fn check_fast<const FIND_DIR: bool>(array: &[i8;8]) -> (bool,u32,u32) {
         let numbers = i8x8::from_array(*array);
         let numbers_shifted = numbers.rotate_elements_left::<1>();
 
-        let count = numbers.simd_ne(i8x8::splat(-1)).to_bitmask().trailing_ones();
+        let count = numbers.simd_ne(i8x8::splat(-128)).to_bitmask().trailing_ones();
 
         let deltas = numbers_shifted - numbers;
 
         let asc_okay = (deltas.simd_le(i8x8::splat(3)) & deltas.simd_gt(i8x8::splat(0))).to_bitmask().trailing_ones();
         let desc_okay = (deltas.simd_ge(i8x8::splat(-3)) & deltas.simd_lt(i8x8::splat(0))).to_bitmask().trailing_ones();
 
-        (asc_okay >= count-1) | (desc_okay >= count-1)
+        ((asc_okay == count-1) | (desc_okay == count-1), asc_okay, desc_okay)
     }
 
     fn midwit_parse(input: &[u8]) -> ([u8;8],usize) {
         let mut i = 0;
         let mut j = 0;
         let mut n = 0;
-        let mut result = [255;8];
+        let mut result = [128;8];
 
         loop {
             let byte = input[i];
@@ -264,24 +264,10 @@ pub mod day2 {
             let res = simd_swizzle!(digits,[0,3,6,9,12,15,18,21]) * u8x8::splat(10);
             let res = res + simd_swizzle!(digits,[1,4,7,10,13,16,19,22]);
 
-            let res = mask_final.select(res,u8x8::splat(255));
+            let res = mask_final.select(res,u8x8::splat(128));
 
             (res.to_array(),len)
         }
-    }
-
-    #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
-    unsafe fn fast_skip(input: &[u8]) -> usize {
-        let newline = u8x32::splat(b'\n');
-
-        let text = if input.len() >= 32 {
-            u8x32::from_slice(input)
-        } else {
-            return midwit_parse(input).1;
-        };
-
-        let len = text.simd_eq(newline).first_set().unwrap();
-        len
     }
 
     #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
@@ -293,7 +279,7 @@ pub mod day2 {
         while bytes.len() > 0 {
             let (nums,len) = fast_parse(bytes);
             
-            let okay = check_fast(&std::mem::transmute(nums));
+            let (okay,_,_) = check_fast(&std::mem::transmute(nums));
     
             if okay {
                 // once one is good, assume all others are good too
@@ -307,8 +293,60 @@ pub mod day2 {
         0
     }
 
+    fn slice_entry(mut entry: [u8;8], n: usize) -> [u8;8] {
+        for i in n..7 {
+            entry[i] = entry[i+1];
+        }
+        entry[7] = 128;
+        entry
+    }
+
     #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
     unsafe fn impl2(input: &str) -> i32 {
-        1
+        let mut bytes = input.as_bytes();
+        let mut count = 0;
+        let mut counts = [0,0,0,0];
+
+        while bytes.len() > 0 {
+            let (nums,len) = fast_parse(bytes);
+            
+            let (okay,fail1,fail2) = check_fast(&std::mem::transmute(nums));
+    
+            if okay {
+                count += 1;
+            } else {
+                let a1 = slice_entry(nums,fail1 as usize);
+                let a2 = slice_entry(nums,fail2 as usize);
+                let a3 = slice_entry(nums,fail1 as usize + 1);
+                let a4 = slice_entry(nums,fail2 as usize + 1);
+
+                let (okay1,_,_) = check_fast(&std::mem::transmute(a1));
+                let (okay2,_,_) = check_fast(&std::mem::transmute(a2));
+                let (okay3,_,_) = check_fast(&std::mem::transmute(a3));
+                let (okay4,_,_) = check_fast(&std::mem::transmute(a4));
+
+                if okay1 {
+                    counts[0] += 1;
+                }
+                if okay2 {
+                    counts[1] += 1;
+                }
+                if okay3 {
+                    counts[2] += 1;
+                }
+                if okay4 {
+                    counts[3] += 1;
+                }
+
+                if okay1 || okay2 || okay3 || okay4 {
+                    count += 1;
+                }
+            }
+
+            bytes = &bytes[len+1..];
+        }
+
+        println!("? {:?}",counts);
+        count
     }
 }
